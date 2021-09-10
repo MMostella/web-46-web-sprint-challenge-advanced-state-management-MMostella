eff6703f38d767b65c7983d110dfd2eb35f8da1a
MMostella <masonmostella@gmail.com>
2021-09-10 09:55:02 -0500

    Set up reducers and actions

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,8 +1,47 @@
 
+import { FETCH_START, FETCH_SUCCESS, FETCH_FAIL, ADD_SMURF } from "../actions"
+
+
 export const initialState = {
+    smurfs: [],
+    loading: false,
+    error: ''
 }
 
-const reducer = ()=>{
+const reducer = (state = initialState, action) => {
+    switch(action.type){
+        case ( FETCH_START ):
+            return ({
+                ...state,
+                loading: true,
+                error: ''
+            })
+        case ( FETCH_SUCCESS ):
+            return ({
+                ...state,
+                loading: false,
+                smurfs: action.payload,
+                error: ''
+            })
+        case ( FETCH_FAIL ):
+            return ({
+                ...state,
+                error: 'You have a smurfing error!!!'
+            })
+        case ( ADD_SMURF ):
+            return ({
+                ...state,
+                smurfs: [...state.smurfs, {
+                    name: action.payload.name,
+                    nickname: action.payload.nickname,
+                    position: action.payload.position,
+                    description: action.payload.description,
+                    id: action.payload.id
+                }]
+            })
+        default:
+            return(state);
+    }
 }
 
 //**************DO NOT EDIT ANY CODE BEYOND THIS POINT**************//
@@ -13,10 +52,12 @@ export default reducer;
 //  - an array of smurfs
 //  - a boolean indicating if the app is loading
 //  - a string indicating a possible error message
-
 //2. Add in the arguments needed to complete a standard reducer function.
 //3. Add in a reducer case to accomidate the start of a smurf fetch.
 //4. Add in a reducer case to accomidate the successful smurf api fetch.
+
 //5. Add in a reducer cases to accomidate the failed smurf api fetch.
+
 //6. Add in a reducer case to accomidate adding a smurf (including the name, nickname, position, summary and an internally generated id) into your smurf list.
+
 //7. Add in a reducer case that adds in a value to the error message.
